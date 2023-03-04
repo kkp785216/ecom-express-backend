890da2a66b0dba887bebe19869bbc0258caecc9c
Krishna Kumar Prajapati <kkp785216@gmail.com>
2023-03-04 19:51:49 +0530

    feature: update profile with error handling

--- src/controller/userCtrl.ts
+++ src/controller/userCtrl.ts
@@ -24,6 +24,52 @@ const getProfile = expressAsyncHandler(
 );
 
 
+// Get user profile
+const updateProfile = expressAsyncHandler(
+    async (req: Request, res: Response) => {
+        const { id: userId } = req.query;
+        const { firstName, lastName, email, mobile } = req.body;
+
+        /* Error Handling Start */
+        if (!req.query.id) throw new Error("Plz provide userId");
+        if (firstName && firstName.length < 3) throw new Error("FirstName can't be less than 3 characters");
+        if (lastName && lastName.length < 3) throw new Error("LastName can't be less than 3 characters");
+        if (email) {
+            if (!email.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/)) throw new Error("Invalid email address");
+            if (await User.findOne({ _id: userId, email })) throw new Error("Email already in use!");
+            else if (await User.findOne({ _id: { $ne: userId }, email })) throw new Error("Email already exists!");
+        }
+        if (mobile) {
+            if (!mobile.match(/^[0-9]{10}$/)) throw new Error("Invalid mobile no.");
+            if (await User.findOne({ _id: userId, mobile })) throw new Error("Mobile already in use!");
+            else if (await User.findOne({ _id: { $ne: userId }, mobile })) throw new Error("Mobile no. already exists!");
+        }
+        /* Error Handling Complete */
+
+        try {
+            const newValue = Object.fromEntries(
+                Object.entries({ firstName, lastName, email, mobile }).map(([key, value]) => [key, value || null])
+            );
+            const findUser = await User.findOneAndUpdate(
+                { _id: userId },
+                newValue,
+                { new: true }
+            ).select("-password");
+            if (findUser) {
+                // Send User Profile except Password
+                res.send(findUser);
+            }
+            else {
+                // User doesn't exist
+                throw new Error("User doesn't exist");
+            }
+        } catch (error: any) {
+            throw new Error(error);
+        }
+    }
+);
+
+
 // Delete user profile
 const deleteProfile = expressAsyncHandler(
     async (req: Request, res: Response) => {
@@ -45,4 +91,4 @@ const deleteProfile = expressAsyncHandler(
     }
 );
 
-export { getProfile, deleteProfile };
+export { getProfile, deleteProfile, updateProfile };
--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,12 +1,15 @@
 import express from 'express';
-import { deleteProfile, getProfile } from '../controller/userCtrl';
+import { deleteProfile, getProfile, updateProfile } from '../controller/userCtrl';
 
 const router = express.Router();
 
 // request and route for get user Profile
 router.get("/profile", getProfile);
 
-// request and route for login existing user
+// request and route for delete user profile
 router.delete("/profile", deleteProfile);
+
+// request and route for update user profile
+router.put("/profile", updateProfile);
 
 export default router;
