cf5969e7a686f8793c08b387c50f1fedde8da162
Krishna Kumar Prajapati <kkp785216@gmail.com>
2023-03-04 14:10:25 +0530

    feature: login by email and password

--- src/models/userModels.ts
+++ src/models/userModels.ts
@@ -1,7 +1,16 @@
 import mongoose from 'mongoose'; // Erase if already required
 import bcrypt from 'bcrypt';
 
-// Declare the Schema of the Mongo model
+type TypeUser = {
+    firstName: string;
+    lastName?: string;
+    email: string;
+    mobile: string;
+    password: string;
+    isPasswordMatched: (inputPassword: string) => Promise<boolean>;
+}
+
+// Declare the Schema of the User model
 const userSchema = new mongoose.Schema({
     firstName: {
         type: String,
@@ -26,11 +35,17 @@ const userSchema = new mongoose.Schema({
     },
 });
 
-userSchema.pre("save", async function (next) {
+// hash the password before save it to database
+userSchema.pre("save", async function () {
     const saltRounds = 10;
     const salt = await bcrypt.genSalt(saltRounds);
     this.password = await bcrypt.hash(this.password, salt);
 });
 
+// convert plain password to hash password and match it to already saved hash password
+userSchema.methods.isPasswordMatched = async function (inputPassword: string) {
+    return await bcrypt.compare(inputPassword, this.password);
+}
+
 //Export the model
-export default mongoose.model('User', userSchema);
+export default mongoose.model<TypeUser>('User', userSchema);
